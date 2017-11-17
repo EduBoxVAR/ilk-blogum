2f7b428b50c25a273a7e997c4c2535732cb94871
dbdemir <demirbesir@gmail.com>
2017-11-17 07:02:08 +0300

    html template changed.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+def post_list(request):
+    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
